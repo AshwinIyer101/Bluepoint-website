3ecb55f00bf94cdccf808bcbfa39fadb5bec38aa
AshwinIyer101 <145373602+AshwinIyer101@users.noreply.github.com>
2024-02-01 02:56:11 +0000

    Random fix for deployment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,8 +4,8 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 const Footer = ({}) => {
 return (
    <footer className=" h-32 border-t border-gray-200">
-    <MaxWidthWrapper>
-
+    <MaxWidthWrapper className="flex justify-center">
+    <p className="pt-5"> beep boop this is sample text footer</p>
     </MaxWidthWrapper>
    </footer>
 )

3ade74f73614cdc63fcf9732bdaa9ac77e8b8ea4
AshwinIyer101 <145373602+AshwinIyer101@users.noreply.github.com>
2024-02-03 01:31:11 +0000

    next mobile section

--- src/components/dot-computer-blurb.tsx
+++ src/components/dot-computer-blurb.tsx
@@ -1,11 +1,25 @@
+import Image from "next/image"
+
 const DotComputer = () => {
     return (
-       <div className="flex justify-center h-screen align-middle items-center" id="DotComputer">
+       <div className="flex sm:flex-col-reverse lg:flex-row-reverse justify-center h-screen align-middle items-center" id="DotComputer">
+        <div className=" pt-5">
+        <p className=" text-center text-lg">
+        Need the power of large language models but don&apos;t want to send away your documents and data? 
+        </p>
+        <p className=" text-center pt-5 text-lg ">
+        <strong>Use Dot</strong>
+        </p>
+        </div>
         <div>
+        <div className="mt-0 lg:mt-0 mx-auto lg:w-4/6">
+        <img src="/dot desktop.png" alt="App Image" className="w-full h-auto object-cover" />
+      </div>
+        </div>
 
         </div>
 
-       </div>
+       
     )
 }
 
